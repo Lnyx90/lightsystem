3735e3ac3c28e8ce949b9a9b20d8b12775ebeff8
Lnyx90 <lnyx90prive@gmail.com>
2025-05-18 22:35:07 +0700

    Ya gt deh

---
+++ src/js/Stats.js
@@ -0,0 +1,8 @@
+const stats = [
+	{ icon: 'hunger', value: 100, color: 'bg-red-500' },
+	{ icon: 'energi', value: 100, color: 'bg-yellow-300' },
+	{ icon: 'hygine', value: 100, color: 'bg-blue-400' },
+	{ icon: 'happy', value: 100, color: 'bg-pink-400' },
+];
+
+export default stats;
