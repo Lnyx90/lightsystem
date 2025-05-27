2c0d824242d8267f4fa8c37a94e4cb525797da62
felic <feliciaulia24@gmail.com>
2025-05-27 18:47:28 +0700

    Mountain Thingy

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -1,4 +1,3 @@
-
 export const actionZones = {
   lake: [
     {
--- src/js/Stats.js
+++ src/js/Stats.js
@@ -5,4 +5,51 @@ const stats = [
 	{ icon: 'happy', value: 100, color: 'bg-pink-400' },
 ];
 
+function performAction(action) {
+  switch (action) {
+    case 'Enjoy the View':
+    case 'Capture the Moment':
+      updateStat('happiness', +10);
+      break;
+    case 'Rest & Eat Snacks':
+      updateStat('energy', +15);
+      updateStat('hunger', -10);
+      break;
+    case 'Hiking Journaling':
+      updateStat('happiness', +5);
+      break;
+
+    case 'Hiking':
+      updateStat('energy', -10);
+      updateStat('happiness', +5);
+      break;
+    case 'Observe Nature':
+      updateStat('happiness', +10);
+      break;
+    case 'Collect Firewood':
+      updateStat('energy', -5);
+      break;
+    case 'Gather Spring Water':
+      updateStat('hygiene', +10);
+      break;
+
+    case 'Set Up Tent':
+      updateStat('energy', -5);
+      break;
+    case 'Cook Food':
+      updateStat('hunger', -20);
+      break;
+    case 'Build a Campfire':
+      updateStat('energy', -10);
+      break;
+    case 'Talk to Fellow Campers':
+      updateStat('happiness', +15);
+      break;
+
+    default:
+      break;
+  }
+}
+
+
 export default stats;
